b1059ab9985badfeade00664b2e49cc3e984f94a
wendywen20 <wenyimei6699@gmail.com>
2024-08-15 11:43:29 -0400

    BOM

---
+++ bom316.js
@@ -0,0 +1,4 @@
+const app=document.getElementById('app')
+const gameBox=document.createElement('table')
+const randomNum=Math.trunc(Math.random()*100+1) //random() is from 0 to 1, such as 0.67, etc
+console.log(randomNum)
